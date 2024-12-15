72d847e547a498efa6aad559ef4217eb9afbe918
nihat deniz özkan <136701651+bissha2@users.noreply.github.com>
2024-12-15 19:10:54 +0300

    google login actually woks well

---
+++ lcwaikiki/lcwaikiki/Form6.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace lcwaikiki
+{
+    partial class Form6
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dataGridView1 = new DataGridView();
+            textBox1 = new TextBox();
+            menuStrip2 = new MenuStrip();
+            incustomerToolStripMenuItem = new ToolStripMenuItem();
+            cocustomerToolStripMenuItem = new ToolStripMenuItem();
+            coempToolStripMenuItem = new ToolStripMenuItem();
+            button2 = new Button();
+            button1 = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            menuStrip2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(12, 27);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(767, 548);
+            dataGridView1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(819, 139);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(321, 23);
+            textBox1.TabIndex = 2;
+            // 
+            // menuStrip2
+            // 
+            menuStrip2.Items.AddRange(new ToolStripItem[] { incustomerToolStripMenuItem, cocustomerToolStripMenuItem, coempToolStripMenuItem });
+            menuStrip2.Location = new Point(0, 0);
+            menuStrip2.Name = "menuStrip2";
+            menuStrip2.Size = new Size(1168, 24);
+            menuStrip2.TabIndex = 4;
+            menuStrip2.Text = "menuStrip2";
+            // 
+            // incustomerToolStripMenuItem
+            // 
+            incustomerToolStripMenuItem.Name = "incustomerToolStripMenuItem";
+            incustomerToolStripMenuItem.Size = new Size(79, 20);
+            incustomerToolStripMenuItem.Text = "incustomer";
+            incustomerToolStripMenuItem.Click += incustomerToolStripMenuItem_Click;
+            // 
+            // cocustomerToolStripMenuItem
+            // 
+            cocustomerToolStripMenuItem.Name = "cocustomerToolStripMenuItem";
+            cocustomerToolStripMenuItem.Size = new Size(82, 20);
+            cocustomerToolStripMenuItem.Text = "cocustomer";
+            cocustomerToolStripMenuItem.Click += cocustomerToolStripMenuItem_Click;
+            // 
+            // coempToolStripMenuItem
+            // 
+            coempToolStripMenuItem.Name = "coempToolStripMenuItem";
+            coempToolStripMenuItem.Size = new Size(56, 20);
+            coempToolStripMenuItem.Text = "coemp";
+            coempToolStripMenuItem.Click += coempToolStripMenuItem_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(1065, 198);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 5;
+            button2.Text = "update";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(819, 459);
+            button1.Name = "button1";
+            button1.Size = new Size(321, 23);
+            button1.TabIndex = 6;
+            button1.Text = "yeni kulllanıcı kaydı";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // Form6
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1168, 587);
+            Controls.Add(button1);
+            Controls.Add(button2);
+            Controls.Add(textBox1);
+            Controls.Add(dataGridView1);
+            Controls.Add(menuStrip2);
+            MainMenuStrip = menuStrip2;
+            Name = "Form6";
+            Text = "Form6";
+            Load += Form6_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            menuStrip2.ResumeLayout(false);
+            menuStrip2.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DataGridView dataGridView1;
+        private MenuStrip menuStrip1;
+        private TextBox textBox1;
+        private ToolStripMenuItem toolStripMenuItem2;
+        private MenuStrip menuStrip2;
+        private ToolStripMenuItem incustomerToolStripMenuItem;
+        private ToolStripMenuItem cocustomerToolStripMenuItem;
+        private ToolStripMenuItem coempToolStripMenuItem;
+        private Button button2;
+        private Button button1;
+    }
+}
---
+++ lcwaikiki/lcwaikiki/Form6.cs
@@ -0,0 +1,148 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lcwaikiki
+{
+    public partial class Form6 : Form
+    {
+        string connectionString = "Server=localhost;Database=hierarchy;User Id=root;Password=Bisshamon22;";
+        public Form6()
+        {
+            InitializeComponent();
+        }
+
+        private void Form6_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void incustomerToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Veriyi çekmek için bağlantı ve komut
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    string query = @"
+                SELECT DISTINCT
+                    p.FirstName AS 'Name',
+                    p.LastName AS 'Surname',
+                    p.email AS 'Email',
+                    p.PhoneNumber AS 'Phone',
+                    p.PersonId AS 'Person ID',
+                    i.IndividualCustomerId AS 'Individual ID',
+                    i.CorporateCustomerId AS 'Corporate ID',
+                    i.DateOfBirth AS 'Date of Birth'
+                FROM 
+                    Person p
+                JOIN 
+                    Individualcustomer i ON p.PersonId = i.PersonId;
+            ";
+
+                    using (MySqlCommand cmd = new MySqlCommand(query, connection))
+                    {
+                        using (MySqlDataAdapter adapter = new MySqlDataAdapter(cmd))
+                        {
+                            DataTable dataTable = new DataTable();
+                            adapter.Fill(dataTable); // Veriyi DataTable'a doldur
+
+                            // DataGridView'a veriyi bağla
+                            dataGridView1.DataSource = dataTable;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Hata: " + ex.Message);
+            }
+        }
+
+        private void cocustomerToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Veriyi çekmek için bağlantı ve komut
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    string query = @"
+                        SELECT CorporateCustomerId, CompanyName, TaxNumber, Email, PhoneNumber, 
+                               Address, City, Country, CreatedDate
+                        FROM corporatecustomer;";
+
+                    using (MySqlCommand cmd = new MySqlCommand(query, connection))
+                    {
+                        using (MySqlDataAdapter adapter = new MySqlDataAdapter(cmd))
+                        {
+                            DataTable dataTable = new DataTable();
+                            adapter.Fill(dataTable); // Veriyi DataTable'a doldur
+
+                            // DataGridView'a veriyi bağla
+                            dataGridView1.DataSource = dataTable;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Hata: " + ex.Message);
+            }
+        }
+
+        private void coempToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Veriyi çekmek için bağlantı ve komut
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    string query = @"
+                SELECT DISTINCT
+                    p.FirstName AS 'Name',
+                    p.LastName AS 'Surname',
+                    p.email AS 'Email',
+                    p.PhoneNumber AS 'Phone',
+                    p.PersonId AS 'Person ID',
+                    c.EmployeeId AS 'EmployeeId',
+                    c.CorporateCustomerId AS 'Corporate Customer ID',
+                    c.Position,
+                    c.HireDate AS 'Hire Date'
+                    FROM
+                      person p
+                    JOIN
+                      corporateemployee c ON p.personId = c.personId;               
+";
+                    using (MySqlCommand cmd = new MySqlCommand(query, connection))
+                    {
+                        using (MySqlDataAdapter adapter = new MySqlDataAdapter(cmd))
+                        {
+                            DataTable dataTable = new DataTable();
+                            adapter.Fill(dataTable); // Veriyi DataTable'a doldur
+
+                            // DataGridView'a veriyi bağla
+                            dataGridView1.DataSource = dataTable;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Hata: " + ex.Message);
+            }
+        }
+    }
+}
